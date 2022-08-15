a763f9443d2fbee1588f7b2c639b99aa5730ec9c
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-08-15 16:16:24 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "tags" {
   description = "A map of tags to add to all resources"
   type        = map(string)
 }
+
+variable "enabled" {
+  description = "Set to false to prevent the module from creating anything"
+  default     = true
+}
