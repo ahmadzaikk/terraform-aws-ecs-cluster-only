4c371be87ca0fdcc2f5c78e5730d0128c9585931
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-08-15 16:15:24 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
+
+module "enabled" {
+  source  = "git::https://git@github.com/ahmadzaikk/terraform-local-boolean.git"
+  version = "0.1.2"
+  value   = "${var.enabled}"
+}
+
+ 
 resource "aws_ecs_cluster" "this" {
+  count = "${module.enabled.value}"
   name = join("-", [var.name, "cluster"])
   tags = var.tags
 }
